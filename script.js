9f1f74806a86244f3d30da177f7d12d7a9ed81e1
propgmachine <77746698+propgmachine@users.noreply.github.com>
2021-03-31 19:09:38 -0400

    updated

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log('hello world!')
+
+const bigTitle = document.querySelector('.big-title');
+
+bigTitle.innerHTML = "<div>Element to be styled</div>";
+
+bigTitle.style.fontSize = '100px';
