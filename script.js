6262ae36551671830b78d0e2899afa3967e7616c
propgmachine <77746698+propgmachine@users.noreply.github.com>
2021-03-31 19:40:46 -0400

    updated script.js

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const bigTitle = document.querySelector('.big-title');
 bigTitle.innerHTML = "<div>Element to be styled</div>";
 
 bigTitle.style.fontSize = '100px';
+
+const bigTitle = document.querySelectorAll('.big-title');
+console.log('bigTitles', bigTitles);
+
+const secondBigTitle = bigTitles[1];
+secondBigTitle.style.backgroundColor = 'blue';
+
+const image = document.querySelector('img');
+image.src = ''
+
+const h6 = document.querySelector('#a-header');
+console.log(h6);
